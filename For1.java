f3c7eb4eb5d06ba62a6f7aaf194ef69f2cd9e1bb
chiradeep2006 <chiradeep032@gmail.com>
2026-01-21 21:46:16 +0530

    Add files via upload

---
+++ For1.java
@@ -0,0 +1,10 @@
+
+public class For1 {
+    public static void main(String[] args) {
+        int i=10;
+        for(int a=i;a>0;a--){
+            System.out.println(i);
+        }
+        System.out.println("HAPPY NEW YAER!");
+    }
+}
