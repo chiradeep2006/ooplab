a1a1dd1f803a9c8c090f2eec7f8d2e31f76c2f09
chiradeep2006 <chiradeep032@gmail.com>
2026-02-21 21:45:16 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog=new Dog();
+        Cat cat=new Cat();
+        dog.bark();
+        cat.meow();
+        System.out.println(dog.isalive);
+          System.out.println(cat.isalive);
+          System.out.println(dog.lives);
+          System.out.println(cat.lives);
+        }
+    
+}
