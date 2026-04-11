2aefc94a9339facf446f3ed7ceb0bdcdc3bdf86a
chiradeep2006 <chiradeep032@gmail.com>
2026-04-11 22:00:12 +0530

    Add files via upload

---
+++ override.java
@@ -0,0 +1,25 @@
+ interface Flyer {
+void fly ();
+}
+ interface Swimmer {
+void swim ();
+}
+class Duck implements Flyer,Swimmer {
+    @Override
+public void fly(){
+    System.out.println("DUCK FLYS");
+}
+@Override
+    public void swim(){
+        System.out.println("DUCK SWIMS");
+    }
+}
+public class override{
+    public static void main(String[]args){
+        Duck d = new Duck();
+        d.fly();
+        d.swim();
+    }
+}
+   
+
